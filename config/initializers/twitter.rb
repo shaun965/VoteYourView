e8bea5d70c630edd1db46c5761f9641a00c0c6cb
shaun <shaun965@hotmail.com>
2015-07-09 17:27:29 -0700

    Minor change

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,10 +1,2 @@
-CLIENT = Twitter::REST::Client.new do |config|
-  config.consumer_key        = Rails.application.secrets.twitter_consumer_key
-  config.consumer_secret     = Rails.application.secrets.twitter_consumer_secret
-  config.access_token        = Rails.application.secrets.twitter_access_token
-  config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
-end
-
-CLIENT.bearer_token
 
 #Gibbon::API.api_key = Rails.application.secrets.mailchimp_api_key
