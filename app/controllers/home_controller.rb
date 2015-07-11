3fecd524e39e174fc299ca1107e77bdb5f06f172
shaun <shaun965@hotmail.com>
2015-07-11 13:03:36 -0700

    Added basic sorting based on the amount of total votes per question

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+    
     @question = Question.all
+    @question = @question.sort! { |a,b| (b.vote_details.where(yes: true).count+b.vote_details.where(yes: false).count) <=> (a.vote_details.where(yes: true).count+a.vote_details.where(yes: false).count) }
+    binding.pry
     @ip = request.remote_ip
     # binding.pry 
   end
